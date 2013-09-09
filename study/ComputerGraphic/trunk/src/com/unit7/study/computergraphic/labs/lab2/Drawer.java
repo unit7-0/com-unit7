f871a2f217498541e729b2a01eb0c4502f21dce4
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-09-09 16:29:31 +0000

    import schema

---
+++ study/ComputerGraphic/trunk/src/com/unit7/study/computergraphic/labs/lab2/Drawer.java
@@ -0,0 +1,47 @@
+package com.unit7.study.computergraphic.labs.lab2;
+
+import javax.media.opengl.GL2;
+
+/**
+ * Рисовальщик.
+ * Передает управление реальному алгоритму.
+ * @author unit7
+ *
+ */
+public class Drawer {
+    public Drawer() {
+        this(new DrawerStrategy() {
+            @Override
+            public void display(GL2 gl) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void clear(GL2 gl) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+
+    public Drawer(DrawerStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void clear(GL2 gl) {
+        strategy.clear(gl);
+    }
+
+    public void display(GL2 gl) {
+        strategy.display(gl);
+    }
+
+    public DrawerStrategy getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(DrawerStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    private DrawerStrategy strategy;
+}
