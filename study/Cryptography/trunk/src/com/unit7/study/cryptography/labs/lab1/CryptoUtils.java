ae26415704e23991b3ad2bc23629f48e9a09d9f6
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-09-11 12:52:21 +0000

    add tests

--- study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab1/CryptoUtils.java
+++ study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab1/CryptoUtils.java
@@ -1,6 +1,8 @@
 package com.unit7.study.cryptography.labs.lab1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CryptoUtils {
@@ -11,7 +13,7 @@ public class CryptoUtils {
      * @param p модуль
      * @return ключ для шифрования
      */
-    public static long DiffieHelmanKey(int priv, int pub, long p) {
+    public static long DiffieHelmanKey(long priv, long pub, long p) {
         return MathUtils.binpow(pub, priv, p);
     }
     
@@ -21,24 +23,26 @@ public class CryptoUtils {
      * @param a число
      * @param key число после возведения в степень
      * @param p модуль
-     * @return x, where x = log<sub>a</sub>(key). a<sup>x</sup> mod p = key;
+     * @return Список значений x, где x = log<sub>a</sub>(key). a<sup>x</sup> mod p = key; 
      */
-    public static long babyStepGiantStep(int a, long key, long p) {
+    public static List<Long> babyStepGiantStep(long a, long key, long p) {
     	Map<Long, Integer> babySteps = new HashMap<Long, Integer>();
         int m = (int) Math.sqrt(p) + 1;
         for (int i = m - 1; i >= 0; --i) {
             babySteps.put(MathUtils.binpow(a, i, p) * key % p, i);
         } 
 
-        for (long t = m, i = 1; t <= m * m; ++i, t *= i) {
+        List<Long> result = new ArrayList<Long>();
+        for (long t = m, i = 1; t <= m * m; ++i, t += m) {
             long b = MathUtils.binpow(a, t, p);
             if (babySteps.containsKey(b)) {
                 long x = i * m - babySteps.get(b);
-                return x;
+                result.add(x);
             }
         }
 
-    	// TODO check it
-    	throw new IllegalArgumentException("May be something wrong in arguments, because the solution must be works for all samples.");
+        if (result.size() == 0)
+        	throw new IllegalArgumentException("May be something wrong in arguments, because the solution must be works for all samples");
+        return result;
     }
 }
