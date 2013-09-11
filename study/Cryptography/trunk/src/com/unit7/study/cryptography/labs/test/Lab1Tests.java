00aaecff70a26cc2b59aa0fb54c9166374d006bb
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-09-11 15:12:57 +0000

    change test testBinpow

--- study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/test/Lab1Tests.java
+++ study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/test/Lab1Tests.java
@@ -2,6 +2,7 @@ package com.unit7.study.cryptography.labs.test;
 
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Random;
 
 import com.unit7.study.cryptography.labs.lab1.CryptoUtils;
 import com.unit7.study.cryptography.labs.lab1.MathUtils;
@@ -12,88 +13,102 @@ import junit.framework.TestCase;
 
 public class Lab1Tests extends TestCase {
     public void testBinpow() {
-    	writer.println("testBinpow() [started]");
-    	
-    	int a = 100;
-    	int d = 4;
-    	int mod = 150;
-    	int res = (int) MathUtils.binpow(a, d, mod);
-    	
-    	int res2 = ((int) Math.pow(a, d)) % mod;
-    	
-    	assertTrue(res == res2);
-    	
-    	writer.println(a + "^" + d + "%" + mod + "=" + res);
-    	
-    	writer.println("testBinpow() [started]");
-    }
-    
-    public void testGcd() {
+        writer.println("testBinpow() [started]\n");
+
+        Random rand = new Random(System.currentTimeMillis());
+        for (int i = 0; i < 10; ++i) {
+            int a = rand.nextInt(50);
+            int d = rand.nextInt(50);
+            int mod = rand.nextInt(100000);
+            int res = (int) MathUtils.binpow(a, d, mod);
+            writer.println(a + "^" + d + "%" + mod + "=" + res);
+        }
         
+        writer.println("\ntestBinpow() [started]");
     }
-    
+
+    public void testGcd() {
+
+    }
+
     public void testDiffieHellman() {
         writer.println("\ntestDiffieHelman() [started]\n");
         int q = Utils.getRandPrime(5777);
         int p = 2 * q + 1;
-        
+
         int g = 2;
         while (MathUtils.binpow(g, q, p) == 1) {
-        	for (int i = g + 1; i < p; ++i) {
-        		if (Utils.isPrime(i)) {
-        			g = i;
-        			break;
-        		}
-        	}
+            for (int i = g + 1; i < p; ++i) {
+                if (Utils.isPrime(i)) {
+                    g = i;
+                    break;
+                }
+            }
         }
-        
+
         CryptoData cryptoDataFirst = new CryptoData(p, g);
         CryptoData cryptoDataSecond = new CryptoData(p, g);
-        writer.println(String.format("q: %d, p: %d, g: %d\nprivate A: %d, public A: %d\nprivate B: %d, public B: %d", 
-                                        q, cryptoDataFirst.getP(), cryptoDataFirst.getG(), cryptoDataFirst.getPrivate(),
-                                        cryptoDataFirst.getPublic(), cryptoDataSecond.getPrivate(), cryptoDataSecond.getPublic()));
-        
-        long keyAB = CryptoUtils.DiffieHelmanKey(cryptoDataFirst.getPrivate(), cryptoDataSecond.getPublic(), p);
-        long keyBA = CryptoUtils.DiffieHelmanKey(cryptoDataSecond.getPrivate(), cryptoDataFirst.getPublic(), p);
-        
+        writer.println(String
+                .format("q: %d, p: %d, g: %d\nprivate A: %d, public A: %d\nprivate B: %d, public B: %d",
+                        q, cryptoDataFirst.getP(), cryptoDataFirst.getG(),
+                        cryptoDataFirst.getPrivate(),
+                        cryptoDataFirst.getPublic(),
+                        cryptoDataSecond.getPrivate(),
+                        cryptoDataSecond.getPublic()));
+
+        long keyAB = CryptoUtils.DiffieHelmanKey(cryptoDataFirst.getPrivate(),
+                cryptoDataSecond.getPublic(), p);
+        long keyBA = CryptoUtils.DiffieHelmanKey(cryptoDataSecond.getPrivate(),
+                cryptoDataFirst.getPublic(), p);
+
         assertTrue(keyAB == keyBA);
 
-        writer.println("Cypher key: " + keyAB + "\n\ntestDiffieHellman() [finished]\n");
+        writer.println("Cypher key: " + keyAB
+                + "\n\ntestDiffieHellman() [finished]\n");
     }
-    
+
     public void testBabyStepGiantStep() {
-    	writer.println("\ntestBabyStepGianStep() [started]\n");
-    	
-    	int q = Utils.getRandPrime(5777);
+        writer.println("\ntestBabyStepGianStep() [started]\n");
+
+        int q = Utils.getRandPrime(5777);
         int p = 2 * q + 1;
-        
+
         int g = Utils.getRandPrime(p);
         while (MathUtils.binpow(g, q, p) == 1) {
-        	for (int i = g + 1; i < p; ++i) {
-        		if (Utils.isPrime(i)) {
-        			g = i;
-        			break;
-        		}
-        	}
+            for (int i = g + 1; i < p; ++i) {
+                if (Utils.isPrime(i)) {
+                    g = i;
+                    break;
+                }
+            }
         }
-        
+
         CryptoData cryptoDataFirst = new CryptoData(p, g);
         CryptoData cryptoDataSecond = new CryptoData(p, g);
-        
-        long keyAB = CryptoUtils.DiffieHelmanKey(cryptoDataFirst.getPrivate(), cryptoDataSecond.getPublic(), p);
-        long keyBA = CryptoUtils.DiffieHelmanKey(cryptoDataSecond.getPrivate(), cryptoDataFirst.getPublic(), p);
-        
+
+        long keyAB = CryptoUtils.DiffieHelmanKey(cryptoDataFirst.getPrivate(),
+                cryptoDataSecond.getPublic(), p);
+        long keyBA = CryptoUtils.DiffieHelmanKey(cryptoDataSecond.getPrivate(),
+                cryptoDataFirst.getPublic(), p);
+
         assertTrue(keyAB == keyBA);
-    	
-    	List<Long> privateA = CryptoUtils.babyStepGiantStep(cryptoDataSecond.getPublic(), keyAB, p);
-    	List<Long> privateB = CryptoUtils.babyStepGiantStep(cryptoDataFirst.getPublic(), keyAB, p);
-    	
-    	writer.println("PrivateA: " + cryptoDataFirst.getPrivate() + " decyphered: " + privateA.contains((long) cryptoDataFirst.getPrivate()) 
-    			+ "\nPrivateB: " + cryptoDataSecond.getPrivate() + " decyphered: " + privateB.contains((long) cryptoDataSecond.getPrivate()));
-    	
-    	writer.println("Decyphering keys:\nPrivateA: " + privateA + "\nPrivateB: " + privateB);
-    	writer.println("\ntestBabyStepGiatStep() [finished]");
+
+        List<Long> privateA = CryptoUtils.babyStepGiantStep(
+                cryptoDataSecond.getPublic(), keyAB, p);
+        List<Long> privateB = CryptoUtils.babyStepGiantStep(
+                cryptoDataFirst.getPublic(), keyAB, p);
+
+        writer.println("PrivateA: " + cryptoDataFirst.getPrivate()
+                + " decyphered: "
+                + privateA.contains((long) cryptoDataFirst.getPrivate())
+                + "\nPrivateB: " + cryptoDataSecond.getPrivate()
+                + " decyphered: "
+                + privateB.contains((long) cryptoDataSecond.getPrivate()));
+
+        writer.println("Decyphering keys:\nPrivateA: " + privateA
+                + "\nPrivateB: " + privateB);
+        writer.println("\ntestBabyStepGiatStep() [finished]");
     }
-    
+
     private PrintWriter writer = new PrintWriter(System.out, true);
 }
