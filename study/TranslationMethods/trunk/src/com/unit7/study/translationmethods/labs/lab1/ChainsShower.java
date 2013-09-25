1f08d66c6369eb2ebef889f26c2d426da0d06678
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-09-25 07:01:41 +0000

    just a second

--- study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/ChainsShower.java
+++ study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/ChainsShower.java
@@ -30,35 +30,38 @@ public class ChainsShower extends JFrame {
 	}
 
 	/**
-	 * build chains
-	 * TODO improve asymptotic
+	 * build chains TODO improve asymptotic
+	 * 
 	 * @param expr
 	 * @param pos
 	 * @return
 	 */
 	private List<String> doBuild(String expr, int pos) {
 		List<String> totalResult = new ArrayList<String>();
-    	StringBuilder result = new StringBuilder(expr.substring(0, pos - 1));
-    	// step by step through expression if we met notTerminal then buid all possible chains
-    	for (int i = pos; i < expr.length(); ++i) {
-    		if (GrammarRules.isTerminal(expr.charAt(i))) {
-    			result.append(expr.charAt(i));
-    		} else if (GrammarRules.isNotTerminal(expr.charAt(i))) {
-    			// possible chains
-    			String[] possibleChains = expandChain(expr.substring(i, i + 1));
-    			// each chain may present as before = result[pos..i - 1], 
-    			// current = one of chains[]
-    			// and after = before + each of chains[] + remain expression(expr)
-    			for (String possible : possibleChains) {
-    				List<String> chains = doBuild(result.toString() + possible + expr.substring(i + 1), result.length());
-    				totalResult.addAll(chains);
-    			}
-    		}
-    	}
-    	
-    	totalResult.add(result.toString());
-    	return totalResult;
-    }
+		StringBuilder result = new StringBuilder(expr.substring(0, pos - 1));
+		// step by step through expression if we met notTerminal then buid all
+		// possible chains
+		for (int i = pos; i < expr.length(); ++i) {
+			if (GrammarRules.isTerminal(expr.charAt(i))) {
+				result.append(expr.charAt(i));
+			} else if (GrammarRules.isNotTerminal(expr.charAt(i))) {
+				// possible chains
+				String[] possibleChains = expandChain(expr.substring(i, i + 1));
+				// each chain may present as before = result[pos..i - 1],
+				// current = one of chains[]
+				// and after = before + each of chains[] + remain
+				// expression(expr)
+				for (String possible : possibleChains) {
+					List<String> chains = doBuild(result.toString() + possible
+							+ expr.substring(i + 1), result.length());
+					totalResult.addAll(chains);
+				}
+			}
+		}
+
+		totalResult.add(result.toString());
+		return totalResult;
+	}
 
 	private String[] expandChain(String chain) {
 		return expressions.get(chain).split(GrammarRules.GRAMMAR_DELIMETER);
