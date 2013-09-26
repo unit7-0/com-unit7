550e6f3078f79e386cf9cc5f8614fd933798fb3e
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-09-26 10:22:42 +0000

    som fixes

--- study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/GrammarRules.java
+++ study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/GrammarRules.java
@@ -4,7 +4,7 @@ public class GrammarRules {
     public static final String TERMINALS = "abcdefghigjklmnopqrstufvwxyz0123456789";
     public static final String NOT_TERMINALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String DELIMETERS = " ,";
-    public static final String GRAMMAR_DELIMETER = "|";
+    public static final String GRAMMAR_DELIMETER = "\\|";
     public static final String GRAMMAR_EMPTY = "&";
     
     public static boolean isTerminal(char target) {
