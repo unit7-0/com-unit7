6acd43d0e53e5ab27191f2c39ddc147d2b092b6b
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-10-14 02:40:36 +0000

    fix digit names

--- study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/GrammarRules.java
+++ study/TranslationMethods/trunk/src/com/unit7/study/translationmethods/labs/lab1/GrammarRules.java
@@ -1,7 +1,7 @@
 package com.unit7.study.translationmethods.labs.lab1;
 
 public class GrammarRules {
-    public static final String TERMINALS = "abcdefghigjklmnopqrstufvwxyz0123456789";
+    public static final String TERMINALS = "abcdefghigjklmnopqrstufvwxyz()";
     public static final String NOT_TERMINALS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String DELIMETERS = " ,";
     public static final String PURE_GRAMMAR_DELIMITER = "|";
