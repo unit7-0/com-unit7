44302015614a669222864b0dacd03821a80ef953
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-11-21 16:12:42 +0000

    some changes

--- study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab4/Card.java
+++ study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab4/Card.java
@@ -2,4 +2,16 @@ package com.unit7.study.cryptography.labs.lab4;
 
 public class Card {
 
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
 }
