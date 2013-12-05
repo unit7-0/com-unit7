71da48f6a2eb6a2d7124c2817f97633f6f4f6da9
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-05 17:48:50 +0000

    may be last commit...

--- study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab6/tests/Lab6Tests.java
+++ study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab6/tests/Lab6Tests.java
@@ -1,7 +1,17 @@
 package com.unit7.study.cryptography.labs.lab6.tests;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.unit7.study.cryptography.labs.lab6.Alice;
@@ -9,17 +19,19 @@ import com.unit7.study.cryptography.labs.lab6.Bob;
 import com.unit7.study.cryptography.labs.lab6.GraphObject;
 import com.unit7.study.cryptography.labs.lab6.interfaces.Question;
 import com.unit7.study.cryptography.labs.lab6.interfaces.VerificationData;
+import com.unit7.study.cryptography.tools.Pair;
 
 public class Lab6Tests {
 	@Test
 	public void tryVerify() {
-		int n = 0;
-		int m = 0;
-		// read
+		int n = nextInt();
+		int m = nextInt();
 		
 		int[][] g = readGraph(n, m);
+		List<Pair<Integer, Integer>> cycle = readCycle(n);
+		
 		GraphObject object = new GraphObject();
-		Alice alice = new Alice(object);
+		Alice alice = new Alice(object, cycle);
 		Bob bob = new Bob();
 		
 		VerificationData data = alice.beginVerification();
@@ -38,16 +50,51 @@ public class Lab6Tests {
 		writer.println("verification: " + verified);
 	}
 	
-	private int[][] readGraph(int n, int m) {
+	private List<Pair<Integer, Integer>> readCycle(int n) {
+	    List<Pair<Integer, Integer>> cycle = new ArrayList<Pair<Integer, Integer>>();
+	    for (int i = 0; i < n; ++i) {
+	        cycle.add(new Pair(nextInt(), nextInt()));
+	    }
+	    
+        return cycle;
+    }
+
+    private int[][] readGraph(int n, int m) {
 		int[][] g = new int[n][m];
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < m; ++j) {
-				
+				int first = nextInt() - 1;
+				int second = nextInt() - 1;
+				g[first][second] = 1;
+				g[second][first] = 1;
 			}
 		}
 		
 		return g;
 	}
 	
+	private int nextInt() {
+	    try {
+            while (tokenizer == null || !tokenizer.hasMoreTokens())
+                tokenizer = new StringTokenizer(reader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+	    
+	    return Integer.parseInt(tokenizer.nextToken());
+	}
+	
+	@Before
+	public void prepare() {
+	    try {
+            reader = new BufferedReader(new FileReader("/home/unit7/graph.txt"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            Assert.fail(e.getLocalizedMessage());
+        }
+	}
+	
 	private PrintWriter writer = new PrintWriter(System.out);
+	private BufferedReader reader;
+	private StringTokenizer tokenizer;
 }
