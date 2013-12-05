df7645992daf5fbb5a0d33fa01bb195c1834df19
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-05 06:04:35 +0000

    some implementations

--- study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab6/interfaces/Verifier.java
+++ study/Cryptography/trunk/src/com/unit7/study/cryptography/labs/lab6/interfaces/Verifier.java
@@ -1,4 +1,4 @@
-package com.unit7.study.cryptography.labs.lab6;
+package com.unit7.study.cryptography.labs.lab6.interfaces;
 
 public interface Verifier {
 	Question verifySubject(VerificationData data);
