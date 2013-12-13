d1277161b6a6c2e9488e0ce4f62b83def6aa5608
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-13 04:34:50 +0000

    implemented type of card combination determining

--- PokerClient/com/unit7/services/pokerservice/client/model/Suit.java
+++ PokerClient/com/unit7/services/pokerservice/client/model/Suit.java
@@ -1,5 +1,11 @@
 package com.unit7.services.pokerservice.client.model;
 
+import com.unit7.services.pokerservice.client.tools.Utils;
+
 public enum Suit {
-    SPADES, CLUBS, DIAMONDS, HEARTS
+    SPADES, CLUBS, DIAMONDS, HEARTS;
+    
+    public static Suit rand() {
+    	return values()[Utils.getRandInt(values().length)];
+    }
 }
