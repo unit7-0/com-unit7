2c8585cf92ff186850f14de8dbed469b7200a748
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-17 11:41:24 +0000

    \

---
+++ PokerClient/com/unit7/services/pokerservice/client/commands/CommandType.java
@@ -0,0 +1,16 @@
+/**
+ * Date:	17 дек. 2013 г.
+ * File:	CommandType.java
+ *
+ * Author:	Zajcev V.
+ */
+
+package com.unit7.services.pokerservice.client.commands;
+
+/**
+ * @author unit7
+ *
+ */
+public enum CommandType {
+    REQUEST_NAME, GAMERS_INFO, INIT_GAME
+}
---
+++ PokerClient/com/unit7/services/pokerservice/client/commands/app/gui/AbstractForm.java
@@ -0,0 +1,14 @@
+/**
+ * Date:	13.12.2013:18:25:02
+ * File:	AbstractForm.java
+ * 
+ * Author:	Zajcev V.
+ */
+
+package com.unit7.services.pokerservice.client.commands.app.gui;
+
+import javax.swing.JFrame;
+
+public interface AbstractForm {
+	JFrame createGUI();
+}
