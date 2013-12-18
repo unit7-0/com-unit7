fc64e9e5ca7c4be26af7ea49c57b01f5e5ea67fb
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-18 02:05:14 +0000

    created

---
+++ PokerClient/src/com/unit7/services/pokerservice/client/engine/transfer/RequestImpl.java
@@ -0,0 +1,28 @@
+package com.unit7.services.pokerservice.client.engine.transfer;
+
+import java.net.Socket;
+
+public class RequestImpl implements Request {
+    @Override
+    public Socket getSocket() {
+        return socket;
+    }
+
+    @Override
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public Object getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    private Socket socket;
+    private Object data;
+}
