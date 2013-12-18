bc364ed8af054314d9651f2ff0c8cef91a407b6a
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-18 02:14:36 +0000

    f

--- PokerClient/src/com/unit7/services/pokerservice/client/commands/containers/CommandContainer.java
+++
@@ -1,8 +0,0 @@
-package com.unit7.services.pokerservice.client.commands.containers;
-
-import java.io.Serializable;
-
-public interface CommandContainer extends Serializable {
-    void setType(CommandContainerType type);
-    CommandContainerType getType();
-}
--- PokerClient/src/com/unit7/services/pokerservice/client/commands/containers/ShowdownCommandContainer.java
+++
@@ -1,6 +0,0 @@
-package com.unit7.services.pokerservice.client.commands.containers;
-
-public class ShowdownCommandContainer extends AbstractCommandContainer {
-
-    private static final long serialVersionUID = -8423588941901354819L;
-}
