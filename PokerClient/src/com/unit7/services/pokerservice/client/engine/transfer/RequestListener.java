39d5af46dd664d862fbef5bfe420dbca8d684a94
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-18 02:14:36 +0000

    f

--- PokerClient/src/com/unit7/services/pokerservice/client/engine/transfer/RequestListener.java
+++
@@ -1,9 +0,0 @@
-package com.unit7.services.pokerservice.client.engine.transfer;
-
-import java.net.Socket;
-
-public interface RequestListener {
-    Response executeRequest(Request request);
-    Object waitMessage(Socket sock);
-    void sendMessage(Socket sock, Object data);
-}
