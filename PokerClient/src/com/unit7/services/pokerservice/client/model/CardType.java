f75a1068acf14ccd9ee750f2e7fc2ccd1542a7aa
vialurt <vialurt@8dae9c98-9cfa-19cd-7016-99b4da0f52a5>
2013-12-21 16:10:27 +0000

    add GUI implementation

--- PokerClient/src/com/unit7/services/pokerservice/client/model/CardType.java
+++ PokerClient/src/com/unit7/services/pokerservice/client/model/CardType.java
@@ -3,7 +3,7 @@ package com.unit7.services.pokerservice.client.model;
 import com.unit7.services.pokerservice.client.tools.Utils;
 
 public enum CardType {
-	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, KNAVE, QUEEN, KING, ACE;
+	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, KNAVE, QUEEN, KING, ACE, UNKNOWN;
 	
 	public static CardType createCardType(Suit suit) {
 	    CardType type = values()[Utils.getRandInt(values().length)];
@@ -13,6 +13,11 @@ public enum CardType {
 	
 	public Suit getSuit() {
 	    return suit;
+	}
+	
+	public String getName() {
+	    // stub
+	    return null;
 	}
 	
 	private Suit suit;
